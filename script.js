33223bf3d2f3d36e3c058076afb39660e8c23cde
Bayan Saparbayeva <saparbayevabt@gmail.com>
2023-03-09 21:50:43 -0800

    Creating Heat Map

---
+++ script.js
@@ -0,0 +1,124 @@
+let temperature_url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+let req = new XMLHttpRequest();
+
+let baseTemp
+let values = [];
+
+let xScale;
+let yScale;
+
+let minYear;
+let maxYear;
+
+let width = 1200;
+let height = 600;
+let padding = 60;
+
+let canvas = d3.select('#canvas');
+
+canvas.attr('width', width);
+canvas.attr('height', height);
+
+let generateScale = () => {
+    
+    minYear = d3.min(values, (item) => {
+        return item['year'];
+    })
+    maxYear = d3.max(values, (item) => {
+        return item['year'];
+    })
+
+    xScale = d3.scaleLinear()
+                    .domain([minYear, maxYear + 1])
+                    .range([padding, width - padding]);
+
+    yScale = d3.scaleTime()
+                    .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
+                    .range([padding, height - padding]);
+};
+
+let drawCells = () => {
+
+    let tooltip = d3.select('#tooltip');
+
+    canvas.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class', 'cell')
+        .attr('fill', (item) => {
+            const variance = item['variance']
+            if (variance <= -1) {
+                return 'SteelBlue';
+            } else if (variance <= 0) {
+                return 'LightSteelBlue';
+            } else if (variance <= 1) {
+                return 'Orange';
+            } else {
+                return 'Crimson';
+            }
+        })
+        .attr('data-year', (item) => {
+            return item['year'];
+        })
+        .attr('data-month', (item) => {
+            return item['month'] - 1;
+        })
+        .attr('data-temp', (item) => {
+            return baseTemp + item['variance'];
+        })
+        .attr('width', (width - 2*padding)/(maxYear - minYear))
+        .attr('height', (height - 2*padding)/12)
+        .attr('x', (item) => {
+            return xScale(item['year']);
+        })
+        .attr('y', (item) => {
+            return yScale(new Date(0, item['month'] - 1, 0, 0, 0, 0, 0));
+        })
+        .on('mouseover', (object, item) => {
+            tooltip.transition()
+                .style('visibility', 'visible');
+
+            let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                    'November', 'December'];
+
+            tooltip.text(item['year'] + ' ' + monthNames[item['month'] - 1] + ' - ' + 
+                (baseTemp + item['variance']) + ' (' + item['variance'] + ')');
+            
+            tooltip.attr('data-year', item['year']);
+        })
+        .on('mouseout', () => {
+            tooltip.transition().style('visibility', 'hidden');
+        });
+};
+
+let generateAxes = () => {
+
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format('d'));
+    let yAxis = d3.axisLeft(yScale)
+                    .tickFormat(d3.timeFormat('%B'));
+
+    canvas.append('g')
+        .call(xAxis)
+        .attr('id', 'x-axis')
+        .attr('transform', 'translate(0, ' + (height - padding) + ')');
+
+    canvas.append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(' + padding + ', 0)');
+
+};
+
+req.open('GET', temperature_url, true);
+req.onload = () => {
+    let object = JSON.parse(req.responseText);
+    baseTemp = object['baseTemperature'];
+    values = object['monthlyVariance']
+
+    generateScale();
+    drawCells();
+    generateAxes();
+}
+req.send();
